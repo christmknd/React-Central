4f178ef6f2f7644f2bff53c1d9c7e3ea26134246
christmknd <christmokonda@gmail.com>
2024-02-24 00:29:11 +0100

    Project DONE

--- react-central/src/App.jsx
+++ react-central/src/App.jsx
@@ -12,25 +12,29 @@ import PasswordGenerator from "./views/javascript/PasswordGenerator";
 import TemperatureConvertor from "./views/maths/TemperatureConverter";
 import Time from "./views/javascript/Time";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/ui/Footer";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ageCalc" element={<AgeCalculator />} />
-        <Route path="/bored" element={<BoredApp />} />
-        <Route path="/dice" element={<Dice />} />
-        <Route path="/faussaire" element={<Faussaire />} />
-        <Route path="/imc" element={<ImcCalculator />} />
-        <Route path="/meteo" element={<ParapluieApp />} />
-        <Route path="/passwordGenerator" element={<PasswordGenerator />} />
-        <Route path="/temperature" element={<TemperatureConvertor />} />
-        <Route path="/clock" element={<Time />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </>
+    <div className="app">
+      <div className="nav-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ageCalc" element={<AgeCalculator />} />
+          <Route path="/bored" element={<BoredApp />} />
+          <Route path="/dice" element={<Dice />} />
+          <Route path="/faussaire" element={<Faussaire />} />
+          <Route path="/imc" element={<ImcCalculator />} />
+          <Route path="/meteo" element={<ParapluieApp />} />
+          <Route path="/passwordGenerator" element={<PasswordGenerator />} />
+          <Route path="/temperature" element={<TemperatureConvertor />} />
+          <Route path="/clock" element={<Time />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
