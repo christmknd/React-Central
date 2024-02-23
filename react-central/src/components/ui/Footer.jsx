4ea227d63088bc6a6c73a4627caa62909313a4fa
christmknd <christmokonda@gmail.com>
2024-02-24 00:30:53 +0100

    ADD footer

---
+++ react-central/src/components/ui/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/christmknd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/christ-mbombo-mokonda/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
