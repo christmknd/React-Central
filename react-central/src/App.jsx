2d1c1f635e5de71af22a6eece8094aaf9407bdb2
christmknd <christmokonda@gmail.com>
2024-02-23 19:04:54 +0100

    Add all projects to navigation

--- react-central/src/App.jsx
+++ react-central/src/App.jsx
@@ -1,29 +1,37 @@
-import './App.css';
-import Navbar from './components/ui/Navbar';
-import AgeCalculator from './views/javascript/AgeCalculator';
-import BoredApp from './views/api/BoredApp';
+import "./App.css";
+import Navbar from "./components/ui/Navbar";
+import AgeCalculator from "./views/javascript/AgeCalculator";
+import BoredApp from "./views/api/BoredApp";
+import Dice from "./views/javascript/Dice";
+import Faussaire from "./views/library/Faussaire";
 import Home from "./views/Home";
-import PageNotFound from './views/PageNotFound';
+import ImcCalculator from "./views/maths/ImcCalculator";
+import PageNotFound from "./views/PageNotFound";
 import ParapluieApp from "./views/api/ParapluieApp";
-import PasswordGenerator from "./views/javascript/PasswordGenerator"
-import { Route, Routes } from "react-router-dom"
-
+import PasswordGenerator from "./views/javascript/PasswordGenerator";
+import TemperatureConvertor from "./views/maths/TemperatureConverter";
+import Time from "./views/javascript/Time";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-    <Routes>
-    <Route path ="/" element={< Home/>} />
-    <Route path='/ageCalc' element={< AgeCalculator/>}/>
-    <Route path='/bored' element={<BoredApp/>}/>
-    <Route path='/parapluie'  element={<ParapluieApp/>}/>
-    <Route path='/passwordGenerator'  element={<PasswordGenerator/>}/>
-    <Route path="*" element={<PageNotFound />} />
-    </Routes>
-    </>  
-  )
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ageCalc" element={<AgeCalculator />} />
+        <Route path="/bored" element={<BoredApp />} />
+        <Route path="/dice" element={<Dice />} />
+        <Route path="/faussaire" element={<Faussaire />} />
+        <Route path="/imc" element={<ImcCalculator />} />
+        <Route path="/parapluie" element={<ParapluieApp />} />
+        <Route path="/passwordGenerator" element={<PasswordGenerator />} />
+        <Route path="/temperature" element={<TemperatureConvertor />} />
+        <Route path="/clock" element={<Time />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- react-central/src/components/ui/Navbar.jsx
+++ react-central/src/components/ui/Navbar.jsx
@@ -1,18 +1,57 @@
 import { Link } from "react-router-dom";
 
-export default function Navbar () {
-
-    return (
-        <>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/ageCalc">Age Calculator</Link></li>
-                <li><Link to="/bored">Bored App</Link></li>
-                <li><Link to="/parapluie">Parapluie App</Link></li>
-                <li><Link to="/passwordGenerator">Password Generator</Link></li>
-            </ul>
-        </nav>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          Maths
+          <ul>
+            <li>
+              <Link to="/temperature">Temperature</Link>
+            </li>
+            <li>
+              <Link to="/imc">IMC</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          Javascript
+          <ul>
+            <li>
+              <Link to="/ageCalc">Age</Link>
+            </li>
+            <li>
+              <Link to="/dice">Dice</Link>
+            </li>
+            <li>
+              <Link to="/passwordGenerator">Password</Link>
+            </li>
+            <li>
+              <Link to="/clock">Clock</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          API
+          <ul>
+            <li>
+              <Link to="/bored">Bored</Link>
+            </li>
+            <li>
+              <Link to="/meteo">Meteo</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          Library
+          <ul>
+            <li>
+              <Link to="/faussaire">Faussaire</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
 }
